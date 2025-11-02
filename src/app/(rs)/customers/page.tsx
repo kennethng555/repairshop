68059c99ed6f609677f1138dfa2aa90a22e47edb
a <kenng7183@gmail.com>
2025-11-02 01:18:08 +0000

    completed lesson 10

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,5 +1,7 @@
 import CustomerSearch from '@/app/(rs)/customers/CustomerSearch'
 import { getCustomerSearchResults } from '@/lib/queries/getCustomerSearchResults'
+import * as Sentry from "@sentry/nextjs"
+import CustomerTable from "@/app/(rs)/customers/CustomerTable"
 
 export const metadata = {
   title: 'Customer Search',
@@ -14,12 +16,18 @@ export default async function Customers({
 
   if (!searchText) return <CustomerSearch />
 
+  const span = Sentry.startInactiveSpan({
+    name: 'getCustomerSearchResults-2'
+  })
   const results = await getCustomerSearchResults(searchText)
+  span.end()
 
   return (
     <>
       <CustomerSearch />
-      <p>{JSON.stringify(results)}</p>
+      {results.length ? <CustomerTable data={results} /> : (
+        <p className="mt-4">No results found</p>
+      )}
     </>
   )
 }
